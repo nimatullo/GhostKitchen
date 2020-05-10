ff93ae0f0f38b3b20ffbcfbeaee09c816bf6e731
Sherzod Nimatullo <shermanjones99@gmail.com>
2020-05-10 15:20:15 -0400

    changes folder names

--- backend-spring/src/main/java/com/example/ghostkitchen/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.example.ghostkitchen.model;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_OWNER,
-    ROLE_DELIVERY
-}
--- backend-spring/src/main/java/com/example/ghostkitchen/payload/OrderItemRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.ghostkitchen.payload;
-
-public class OrderItemRequest {
-}
